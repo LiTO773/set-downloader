24a27458a994f0fafaba40dea358ef1653607745
LiTO773 <devluisp@hotmail.com>
2023-08-04 23:31:29 +0100

    Adicionado o link do zip

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ import streamlit as st
 import subprocess
 import shutil
 
+show_link = False
+link = ''
+
 def download_and_split(set_link, tracks):
+    show_link = False
+    link = ''
     session_id = str(uuid.uuid4())
 
     # Prepare data for audio-splitter
@@ -61,9 +66,11 @@ def download_and_split(set_link, tracks):
         except subprocess.CalledProcessError as e:
             st.error(f"Erro a zipar:\n{e.output}")
 
-    webbrowser.open_new_tab('google.com')
+    link = f"https://batedores-content.cheeseburgersemqueijo.ovh/{session_id}/output.zip"
 
 st.header("Dunload de sets para membros honorários do batimento do chinelo 🩴")
 link_yt = st.text_input("Link YouTube", placeholder="https://www.youtube.com/watch?v=rxH2q9VhEXM")
 timestamps = st.text_area("Timestamps", placeholder="00:00:05 | ID (Sub Focus & Dimension?) - ID (Intro Dub?)\n00:01:48 | Sub Focus - Trip\n00:03:36 | Sub Focus - Rock It (Wilkinson Remix)\n00:04:18 | Sub Focus & Culture Shock - Recombine")
 st.button("DALEEEEEE ⬇️", use_container_width=True, on_click=download_and_split, args=(link_yt, timestamps))
+if show_link:
+    st.success(f"Zip mítico: {link}")
